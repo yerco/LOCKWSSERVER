a6b194fb809963642d68a58e2594db6ddc4771e9
Yerco Jorquera <yjorquera@Yercos-Mac.local>
2018-05-30 17:50:42 +0200

    feat: ADDED LOGGER TO PUSH SERVER Capability added to inspect the behaviour of the websocket. CHANGELOG - WebsocketServer/PushServer.php & WebsocketServer/ReceiverPusher.php     - Logger injected

--- WebsocketServer/PushServer.php
+++ WebsocketServer/PushServer.php
@@ -9,32 +9,46 @@ use Ratchet\Server\IoServer;
 use Ratchet\WebSocket\WsServer;
 use Ratchet\Wamp\WampServer;
 use ZMQ;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 class PushServer
 {
-
     private $loop;
+    private $logger;
 
-    public function __construct() {
+    public function __construct(Logger $logger = null) {
         $this->loop = Factory::create();
+        $this->logger = $logger;
     }
 
     public function core() {
-        $pusher = new ReceiverPusher();
+        $pusher = new ReceiverPusher($this->logger);
 
         // Listen for the web server to make a ZeroMQ push after an ajax request
         $context = new React\ZMQ\Context($this->loop);
         $pull = $context->getSocket(ZMQ::SOCKET_PULL);
         // Binding to 127.0.0.1 means the only client that can connect is itself
         /* DEVELOPMENT */
-        //$pull->bind('tcp://127.0.0.1:5555');
+        $pull->bind('tcp://127.0.0.1:5555');
         /* PRODUCTION */
         //$pull->bind('tcp://188.166.11.160:5555');
         /* DOCKER */
-        $pull->bind('tcp://127.0.0.1:5556');
+        //$pull->bind('tcp://127.0.0.1:5555');
         $pull->on('message', array($pusher, 'onNewData'));
+
+        if ($this->logger) {
+            $this->logger->info(
+                "PUSHSERVER started at: ".
+                time(),
+                array(
+                    'Human time' =>
+                        gmdate("Y-m-d\TH:i:s\Z", time())
+                )
+            );
+        }
 
         // Set up our WebSocket server for clients wanting real-time updates
         $webSock = new React\Socket\Server('0.0.0.0:8018', $this->loop); //
@@ -54,7 +68,17 @@ class PushServer
 
 }
 
-$push_server = new PushServer();
+// create a log channel
+try {
+    $log = new Logger('name');
+    $log->pushHandler(new StreamHandler('websocket_server.log',
+        Logger::DEBUG));
+}
+catch (\Exception $e) {
+    echo "Logger creation problem: " . $e->getMessage();
+}
+
+$push_server = new PushServer($log);
 $push_server->core();
 
 
--- WebsocketServer/ReceiverPusher.php
+++ WebsocketServer/ReceiverPusher.php
@@ -4,23 +4,36 @@ namespace WebsocketServer;
 
 use Ratchet\ConnectionInterface;
 use Ratchet\Wamp\WampServerInterface;
+use Monolog\Logger;
+
 
 // Web Application Messaging Protocol (check autobahn.js)
 class ReceiverPusher implements WampServerInterface
 {
-    public function __construct() {
+    private $logger;
 
+    public function __construct(Logger $logger = null) {
+        $this->logger = $logger;
     }
 
     /**
      * A lookup of all the topics clients have subscribed to
      */
     protected $subscribedTopics = array(
-        "gateway_record"    => "algo"
+        "gateway_record"    => "gateway JSON Packet"
     );
 
     public function onOpen(ConnectionInterface $conn) {
-        // TODO: Implement onOpen() method.
+        if ($this->logger) {
+            $this->logger->info(
+                "onOpen",
+                array(
+                    'onOpen time, browser connection' =>
+                        gmdate("Y-m-d\TH:i:s\Z", time()),
+                    'remoteAddress' => $conn->remoteAddress
+                )
+            );
+        }
     }
 
     public function onClose(ConnectionInterface $conn) {
@@ -39,6 +52,14 @@ class ReceiverPusher implements WampServerInterface
     public function onSubscribe(ConnectionInterface $conn, $topic) {
         //var_dump($topic->getId()); // this case `gateway_record`
         $this->subscribedTopics[$topic->getId()] = $topic;
+        if ($this->logger) {
+            $this->logger->info(
+                "onSubscribed: ",
+                array(
+                    "topic ID" => $topic->getId()
+                )
+            );
+        }
     }
 
     public function onUnSubscribe(ConnectionInterface $conn, $topic) {
@@ -59,6 +80,17 @@ class ReceiverPusher implements WampServerInterface
     public function onNewData($entry) {
         $data = json_decode($entry, true);
 
+        if ($this->logger) {
+            $this->logger->info(
+                "Event at ".
+                time(),
+                array(
+                    'Human time' =>
+                        gmdate("Y-m-d\TH:i:s\Z", time())
+                )
+            );
+        }
+
         // If the lookup topic object isn't set there is no one to publish to
         if (!array_key_exists($data['category'], $this->subscribedTopics)) {
             //echo "\n\n\nwn regresa\n";
@@ -66,7 +98,39 @@ class ReceiverPusher implements WampServerInterface
         }
 
         $topic = $this->subscribedTopics[$data['category']];
+        if ($this->logger) {
+           $this->logger->info(
+               "Topic",
+               array(
+                   "topic" => $topic,
+                   "Push server reception time" =>
+                       gmdate("Y-m-d\TH:i:s\Z", time())
+               )
+           );
+        }
         // re-send the data to all the clients
-        $topic->broadcast($data);
+        try {
+            if ($this->logger) {
+                $this->logger->info(
+                    "Broadcasting",
+                    array(
+                        "Data sent:" => $data
+                    )
+                );
+            }
+            $topic->broadcast($data);
+        }
+        catch (\Exception $e) {
+            if ($this->logger) {
+                $this->logger->info(
+                    "Broadcast stopped",
+                    array(
+                        "Halt time" =>
+                            gmdate("Y-m-d\TH:i:s\Z", time()),
+                        "message" => $e->getMessage()
+                    )
+                );
+            }
+        }
     }
 }
